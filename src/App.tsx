aeb7f2ff34cea9dc69467fcea3c0c94ebb821fa1
Tatiane <tatiane_213@hotmail.com>
2022-06-22 20:27:28 -0300

    feat: third ignite lab class

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { ApolloProvider } from "@apollo/client"
+import { Router } from "./Router"
+import { client } from "./lib/apollo"
 import { Event } from "./pages/Event"
+import { BrowserRouter } from "react-router-dom"
 
 function App() { 
   return (
-    <Event />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
